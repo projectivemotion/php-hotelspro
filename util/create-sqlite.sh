ce58f96af79fb257c4b269f1fff60736b2985aae
Amado Martinez <amado@projectivemotion.com>
2016-05-24 13:07:37 -0500

    Improved Json Import

--- util/create-sqlite.sh
+++ util/create-sqlite.sh
@@ -3,78 +3,23 @@
 if [ -z "$1" ] ; then
     cat <<EOL
 
-    1. Create a hotels.csv and destinations.csv file
-    2. Pass the filenames to this script
-    3. This script will product a sqlite db for use by script.
-
-    optionally put all json files in the same directory and call this script:
-
+    Usage
     Examples:
 
 EOL
-
-    echo -e "\t$0 hotelpro.sqlite destinations.csv destinations"
-    echo -e "\t$0 hotelpro.sqlite hotels.csv hotels"
     echo -e "\t$0 hotelpro.sqlite jsonfiles/ /path/to/php"
     exit
 fi;
 
 scriptDIR="$( readlink -f "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" )"
-# create schema
-
-
-if [ ! -f "$1" ] ; then
-sqlite3 "$1" <<EOL
-create table hotels (
-    code TEXT PRIMARY KEY ASC,
-    name TEXT,
-    country TEXT,
-    zipcode TEXT,
-    address TEXT,
-    destination TEXT,
-    latitude TEXT,
-    longitude TEXT,
-    currencycode TEXT,
-    stars INTEGER,
-    hotel_type INTEGER);
-
-create table destinations (
-    code TEXT PRIMARY KEY ASC,
-    country TEXT,
-    parent TEXT,
-    name TEXT,
-    latitude TEXT, longitude TEXT
-);
-EOL
-
-    echo "Created schema in $1";
-fi;
-
 
     if [ -d "$2" ]  && [ -x "$3" ]; then
         echo "Importing hotels and destinations from $2.. please wait..";
         sleep 1
         echo -n "Importing hotels.."
-        "$0" "$1" <("$3" -d memory_limit=258M "$scriptDIR/json-tocsv.php" hotel "$2"/hotels-*.json) hotels
+        "$3" -d memory_limit=258M "$scriptDIR/import-json.php" hotels "$1" "$2"/hotels-*.json
         echo -n "Importing destinations.."
-        "$0" "$1" <("$3" -d memory_limit=258M "$scriptDIR/json-tocsv.php" destination "$2"/destinations-*.json) destinations
+        "$3" -d memory_limit=258M "$scriptDIR/import-json.php" destinations "$1" "$2"/destinations-*.json
         echo "All done. $1 is ready for use."
         exit
     fi
-
-    if [ -f "$1" ] && [ $# -lt 3 ] ; then
-        echo "$1 exists. Aborting."
-        exit
-    fi
-
-
-if [[ $# -gt 2 ]] ; then
-
-     sqlite3 "$1" <<EOL
-.mode csv $3
-.separator "\\$"
-.import $2 $3
-EOL
-
-    echo "Imported " "$(echo "select count(*) from $3;" | sqlite3 "$1")" records into "$2"
-fi;
---
+++ util/import-json.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: amado
+ * Date: 5/21/16
+ * Time: 10:13 PM
+ */
+
+function json_tosql($jsonarr, $colnames, PDOStatement $stmt)
+{
+    foreach($jsonarr as $jsonobj)
+    {
+        $vals   =   [];
+        foreach($colnames as $col)
+        {
+            $vals[]     =   trim($jsonobj->$col);
+        }
+        $stmt->execute($vals);
+    }
+}
+
+function printcsv($mode, $filename, PDO $pdo)
+{
+
+    static $stmts   =   [];
+    $columns    =   [
+        'hotels'         =>  ['code', 'name', 'country', 'zipcode', 'address', 'destination', 'latitude', 'longitude', 'currencycode', 'stars', 'hotel_type'],
+        'destinations'   =>  ['code', 'country', 'parent', 'name', 'latitude', 'longitude']
+    ];
+
+    if(!isset($stmts[$mode]))
+    {
+
+        $qstr   =   sprintf('INSERT INTO %s (%s) VALUES ( ? %s);',
+            $mode,
+            implode(', ', $columns[$mode]),
+            str_repeat(', ?', count($columns[$mode])-1)
+        );
+
+        $stmts[$mode]   =   $pdo->prepare($qstr);
+    }
+    $stmt   =   $stmts[$mode];
+
+    switch($mode)
+    {
+        case 'destinations':
+        case 'hotels':
+                json_tosql(
+                    json_decode(file_get_contents($filename)),
+                    $columns[$mode],
+                    $stmt);
+            break;
+    }
+}
+
+if($argc < 4)
+{
+    echo "\nThis prints a csv dump based on a given a list of json filenames:\n\n\tExample:\n\t\t";
+    die("$argv[0] [hotel|destination] hotelpro.sqlite file1.json file2.json\n\n");
+}
+
+$mode   =   $argv[1];
+$fname  =   $argv[2];
+
+
+
+if(!file_exists($fname))
+{
+    $pdo    =   new PDO('sqlite:' . $fname);
+    $tbls   =   [ <<<HDOC
+create table hotels (
+    code TEXT PRIMARY KEY ASC,
+    name TEXT,
+    country TEXT,
+    zipcode TEXT,
+    address TEXT,
+    destination TEXT,
+    latitude TEXT,
+    longitude TEXT,
+    currencycode TEXT,
+    stars INTEGER,
+    hotel_type INTEGER);
+HDOC
+        , <<<HDOC
+create table destinations (
+    code TEXT PRIMARY KEY ASC,
+    country TEXT,
+    parent TEXT,
+    name TEXT,
+    latitude TEXT, longitude TEXT
+);
+HDOC
+    ];
+
+    foreach($tbls as $tsql)
+    {
+        $pdo->query($tsql);
+    }
+}else{
+
+    $pdo    =   new PDO('sqlite:' . $fname);
+}
+
+for($i=3; $i<$argc; $i++)
+{
+    printcsv($mode, $argv[$i], $pdo);
+}
